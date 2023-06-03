a52a38f55f7f94481729d0a5a3f97f96b5269b1c
go-to-k <24818752+go-to-k@users.noreply.github.com>
2023-06-04 00:20:34 +0900

    Initial commit

---
+++ test/elb-other-5xx-alarm-sample.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ElbOther5XxAlarmSample from '../lib/elb-other-5xx-alarm-sample-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/elb-other-5xx-alarm-sample-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ElbOther5XxAlarmSample.ElbOther5XxAlarmSampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
